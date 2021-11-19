0974bbda3e89b7019b8bda8594cead963c515e35
Gavrik Alexandr <few0000@yandex.ru>
2021-11-19 14:21:14 +0300

    Delete dist from repository

--- dist/profileChangeInfo.533dd8fb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".profile").querySelector(".profile-actions .accept-btn").addEventListener("click",(function(){window.location.href=window.location.origin+"/profile.html"}));
-//# sourceMappingURL=profileChangeInfo.533dd8fb.js.map
