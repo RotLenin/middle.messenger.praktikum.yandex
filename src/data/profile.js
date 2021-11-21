18b64b247fa144fef7c4c5c3052f805d00f4bf35
Gavrik Alexandr <few0000@yandex.ru>
2021-11-21 12:10:32 +0300

    Fix some checklist

--- src/data/profile.js
+++ src/data/profile.js
@@ -8,18 +8,18 @@ const profileFields = [
 ]
 
 const profileChangeInfo = [
-    {name : 'Почта', value: 'pochta@yandex.ru'},
-    {name : 'Логин', value: 'ivanivanov'},
-    {name : 'Имя', value: 'Иван'},
-    {name : 'Фамилия', value: 'Иванов'},
-    {name : 'Имя в чате', value: 'Иван'},
-    {name : 'Телефон', value: '+7 (909) 967 30 30'},
+    {name : 'email', value: 'pochta@yandex.ru', label:'Почта', type: 'email'},
+    {name : 'login', value: 'ivanivanov', label:'Логин', type: 'text'},
+    {name : 'first_name', value: 'Иван', label:'Имя', type: 'text'},
+    {name : 'second_name', value: 'Иванов', label:'Фамилия', type: 'text'},
+    {name : 'display_name', value: 'Иван', label:'Имя в чате', type: 'text'},
+    {name : 'phone', value: '7 (909) 967 30 30', label:'Телефон', type: 'tel'},
 ]
 
 const profileChangePassword = [
-    {name : 'Старый пароль', value: '******'},
-    {name : 'Новый пароль', value: '******'},
-    {name : 'Повторите новый пароль', value: '******'},
+    {name : 'oldPassword', value: '123456', label:'Старый пароль', type: 'password'},
+    {name : 'newPassword', value: '123', label:'Новый пароль', type: 'password'},
+    {name : 'reNewPassword', value: '12', label:'Повторите новый пароль', type: 'password'},
 ]
 
 const profile = {
--- src/data/users.js
+++ src/data/users.js
@@ -2,21 +2,23 @@
 const USERS = [
     {
         login : 'RotLenin',
+        avatar : 'avatar',
         password : '123',
         email : 'few0000@yandex.ru',
-        name : 'Alexandr',
-        surname : 'Gavrik',
-        chatName : 'Lenin',
-        tel : '9000000001',
+        first_name : 'Alexandr',
+        second_name : 'Gavrik',
+        display_name : 'Lenin',
+        phone : '9000000001',
     },
     {
         login : 'user',
+        avatar : 'avatar',
         password : 'user',
         email : 'rotlenin@yandex.ru',
-        name : 'Витя',
-        surname : 'Бирюков',
-        chatName : 'Тот самый',
-        tel : '9000000002',
+        first_name : 'Витя',
+        second_name : 'Бирюков',
+        display_name : 'Тот самый',
+        phone : '9000000002',
     },
 ];
 
